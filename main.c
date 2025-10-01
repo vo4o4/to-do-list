df492f176777ff1e53d27d60d566d614a43f49b2
vo4o4 <vytautas.bal4@gmail.com>
2025-10-02 01:15:33 +0300

    skeleton v1

--- main.c
+++ main.c
@@ -9,6 +9,35 @@ struct task
 
 int main()
 {
+    int input;
+    bool validInput = true;
+    char temp;
+
+    printf("Welcome to to-do-list SKELETON v1\n\n");
+
+    printf("Enter 1 to add a task\nEnter 2 edit task completion\nEnter 3 to view all tasks\nEnter 4 to exit to-do-list\n\n>>");
+
+    while (scanf("%d%c", &input, &temp) != 2 && temp != '\n' && (input != 1 || input != 2 || input != 3 || input != 4))
+    {
+        printf("Invalid input\n");
+    }
+
+    switch (input)
+    {
+    case 1:
+        break;
+    case 2:
+
+        break;
+    case 3:
+
+        break;
+    case 4:
+
+        return 0;
+    default:
+        printf("Invalid input");
+    }
 
     return 0;
 }
